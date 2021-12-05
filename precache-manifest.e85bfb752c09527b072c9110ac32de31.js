37e9390d660941cb08ccb589c2f29612416d4816
Bild96 <Bild96@pm.me>
2021-12-05 22:34:20 +0000

    Updates

---
+++ precache-manifest.e85bfb752c09527b072c9110ac32de31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919ae5bad2a02bf0f4a098dbac9be115",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24c3bb859ff0e48f3e55",
+    "url": "./static/css/2.3870cfe2.chunk.css"
+  },
+  {
+    "revision": "fa04e589bd9016f04b59",
+    "url": "./static/css/main.ea20caf2.chunk.css"
+  },
+  {
+    "revision": "24c3bb859ff0e48f3e55",
+    "url": "./static/js/2.771227ec.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "./static/js/2.771227ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa04e589bd9016f04b59",
+    "url": "./static/js/main.c942388a.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
